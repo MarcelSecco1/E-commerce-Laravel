7935d8b6514b53ad3038601c8bd2f0578a77aa3b
MarcelSecco1 <marcelsecco1@gmail.com>
2024-05-27 04:02:49 -0300

    fix: admin access in filament panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -53,7 +55,13 @@ class User extends Authenticatable
         return $this->hasOne(Pessoa::class);
     }
 
-    public function codes(){
+    public function codes()
+    {
         return $this->hasMany(CodePromotion::class);
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->is_admin;
     }
 }
